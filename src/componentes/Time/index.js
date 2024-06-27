b41536e4fd0c3bdb156328816326076c4d4c78cb
Guilherme H. <ghantani@gmail.com>
2024-06-27 14:52:21 -0300

    Final Commit: organo 2.0

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,15 +1,27 @@
-import Colaborador from "../Colaborador"
-import "./Time.css"
+import Colaborador from '../Colaborador'
+import './time.css'
+import hexToRgba from 'hex-to-rgba';
 
-const Time = (props) => {
-    return(
-        (props.colaboradores.length) > 0 ? <section className="time" style={{backgroundColor: props.corSecundaria}}>
-            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
-            <div className="colaboradores">
-                {props.colaboradores.map(colaborador => <Colaborador key={colaborador.nome} nome={colaborador.nome} corDeFundo={props.corPrimaria} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+const Time = ({ time, colaboradores, aoDeletar, mudarCor, aoFavoritar}) => {
+    return (
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
+            <input onChange={evento => mudarCor(evento.target.value, time.id)} value={time.cor} type='color' className='input-cor'></input>
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
+            <div className='colaboradores'>
+                {colaboradores.map((colaborador, indice) => {
+                    return (
+                        <Colaborador 
+                            key={indice} 
+                            colaborador={colaborador} 
+                            corDeFundo={time.cor} 
+                            aoDeletar={aoDeletar}
+                            aoFavoritar={aoFavoritar}
+                        />
+                    )
+                })}
             </div>
         </section>
-        : ''
+
     )
 }
 
